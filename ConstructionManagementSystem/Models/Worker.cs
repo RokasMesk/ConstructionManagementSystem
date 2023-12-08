6a79a37306fe91e65c4f223293e3ae74fc0d113d
RokasMesk <mesk.rokas@gmail.com>
2023-12-08 12:50:10 +0300

    Added Worker model

---
+++ ConstructionManagementSystem/Models/Worker.cs
@@ -0,0 +1,9 @@
+﻿namespace ConstructionManagementSystem.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ContactInformation { get; set; }
+    }
+}
